5eefe8cf48dd67fac0e12028d9a17b841e4292b5
itolkachev <vantrey@yandex.ru>
2024-09-02 21:37:45 +0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,13 @@ async function bootstrap() {
   //подключение глобального валидационного pipe https://docs.nestjs.com/techniques/validation
   app.useGlobalPipes(new ValidationPipe());
 
+  //разрешены запросы с любых доменов
+  app.enableCors({
+    origin: '*', // Разрешает запросы с любых доменов
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Разрешенные методы
+    credentials: true, // Включает передачу cookies
+  });
+
   //получение конфиг сервиса https://docs.nestjs.com/techniques/configuration#using-in-the-maints
   const configService = app.get(ConfigService<ConfigurationType>);
   const port = configService.get('apiSettings.PORT', { infer: true })!;
